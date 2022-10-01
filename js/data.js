e3d82dd06c319c11f0afe30839c7b73710433ec7
Sezer Iltekin <sezer@iltekin.com>
2022-10-01 19:34:05 +0300

    Fix some bugs

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-const quizLimit = 10; // default: 10
+const quizLimit = 2; // default: 10
 const answerTime = 15; //default: 15
 
 const quizData = [
@@ -199,7 +199,7 @@ const quizData = [
         a: "Melekte bana kartpostal göndermiş.",
         b: "Rize de görev yapan bir öğretmen tanıyorum.",
         c: "Kargaların gagaladığı kargolarda bu kutunun içinde mi?",
-        d: "Tahsin Beyde iyi bir yazılımcıdır.",
+        d: "Tahsin Bey de iyi bir yazılımcıdır.",
         correct: "d",
     },
     {
